4287403bd1bf954501587361981c9d12908d0c07
renanfrancosjc <renanfranco.sjc@hotmail.com>
2024-05-23 22:18:50 -0300

    Projeto conta banco pronto

--- .idea/src/ContaTerminal.java
+++ .idea/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal{
     public static void main(String[] args){
+        //ClasseScaner
+        Scanner sc = new Scanner(System.in);
 
+
+        //Exibir mensagem e obter a mensagem
+
+        //xxx.xxx.xxx
+        System.out.println("Numero da conta");
+        int numero = sc.nextInt();
+        //xxx-x
+        System.out.println("Agência");
+        String agencia = sc.nextLine();
+        //Nome Sobrenome
+        System.out.println("Nome Cliente");
+        String nomeCliente = sc.nextLine();
+        //x.xx
+        System.out.println("Saldo");
+        int saldo = sc.nextFloat();
+
+        System.out.println("Ola " + nomeCliente + " sua conta " + numero + " / " + agencia + " Seu saldo é de R$" + saldo);
     }
 }
